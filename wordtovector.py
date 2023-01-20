58ab15d0ce26ae626e6158e6b0efe45636d01eee
bahnee <122479653+bahnee@users.noreply.github.com>
2023-01-20 17:20:00 +0100

    changed word2vec

--- wordtovector.py
+++ wordtovector.py
@@ -1,10 +1,9 @@
 import pandas as ps
-import ast
-from gensim.models import Word2Vec, FastText    #pip install gensim
+import numpy as np
+import gensim
+#https://code.google.com/archive/p/word2vec/
 
 file = None
-dataCorpus = None
-dataCorpusDict = None
 
 #To set the file in the main.
 def setFile(fileP):
@@ -24,38 +23,35 @@ def allWords(file):
     return(dataCorpus)
 
 def allWordsSet(allWords):
-    global dataCorpus
-
     allWordsSet = []
     for word in allWords:
         if word not in allWordsSet:
             allWordsSet.append(word)
     print("dataCorpus length: " + str(len(allWordsSet)))
-    dataCorpus = allWordsSet
+    return allWordsSet
 
-def w2vDict(featureCount):
-    global dataCorpusDict
 
-    allWordsSet(allWords(file))
-    w2v = Word2Vec(list(file["textasList"]), min_count=1, vector_size = featureCount)
-
-    w2vDict = dict.fromkeys(dataCorpus, 0)
-    for comment in file["textasList"]:
-        for word in comment:
-                w2vDict[word] = sum(list(w2v.wv[word]))
-    return w2vDict
-
+#Add the column "w2v" to the file.
 def w2vCol(featureCount):
 
     global file
-    global dataCorpusDict
     
-    dataCorpusDict = w2vDict(featureCount)
+    #Use our model as base. If googles model has a word, use googles vector of the word.
+    w2vOurModel = gensim.models.Word2Vec(file["textasList"], min_count = 1, vector_size = featureCount)
+    w2vGoogle = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin.gz', binary=True)
+    for word in allWordsSet(allWords(file)):
+        try:
+            w2vOurModel.wv[word] = w2vGoogle[word]
+        except:
+            pass
 
+    #create an array and insert each word2Vec Vector
     w2vCol = []
     for comment in file["textasList"]:
-        bow = dict.fromkeys(dataCorpus, 0)
+        comment2vec = []
         for word in comment:
-            bow[word] = dataCorpusDict[word]
-        w2vCol.append(list(bow.values()))
+            comment2vec.append(w2vOurModel.wv[word])
+        
+        #Now create one vector per comment, which is just the average of the words vectors. 
+        w2vCol.append(np.mean(comment2vec, axis = 0))
     file["w2v"] = w2vCol
