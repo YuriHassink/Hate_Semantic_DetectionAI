d55f3350c0eea3929ca5783ab63e4739c3bfd06b
melinasophie <122479522+melinasophie@users.noreply.github.com>
2023-01-20 19:03:00 +0100

    Update Gloves.py

--- Gloves.py
+++ Gloves.py
@@ -2,21 +2,47 @@
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
-import pandas as ps
-import tfIdf
-import ml
+  
+file = None
+dataCorpus = None
+dataCorpusDict = None
 
-x = ps.read_csv("Hatespeech_dataset.csv")
+#To set the file in the main.
+def setFile(fileP):
+    global file
+    file = fileP
+
+#To get the file in the main.
+def getFile():
+    global file
+    return file
+
+def allWords(file):
+    dataCorpus = []
+    for comment in file["textasList"]:
+        for word in comment:
+            dataCorpus.append(word)
+    return(dataCorpus)
+
+def allWordsSet(allWords):
+    global dataCorpus
+
+    allWordsSet = []
+    for word in allWords:
+        if word not in allWordsSet:
+            allWordsSet.append(word)
+    print("dataCorpus length: " + str(len(allWordsSet)))
+    dataCorpus = allWordsSet
 
 # create the dict.
 tokenizer = Tokenizer()
-tokenizer.fit_on_texts(x)
+tokenizer.fit_on_texts(allWordsSet(allWords(file)))
   
 # number of unique words in dict.
 print("Number of unique words in dictionary=", 
       len(tokenizer.word_index))
 print("Dictionary is = ", tokenizer.word_index)
-  
+ 
 # download glove and unzip it in Notebook.
 #!wget http://nlp.stanford.edu/data/glove.6B.zip
 #!unzip glove*.zip
@@ -24,8 +50,7 @@ print("Dictionary is = ", tokenizer.word_index)
 # vocab: 'the': 1, mapping of words with
 # integers in seq. 1,2,3..
 # embedding: 1->dense vector
-def embedding_for_vocab(filepath, word_index,
-                        embedding_dim):
+def embedding_for_vocab(filepath, word_index,embedding_dim):
     vocab_size = len(word_index) + 1
       
     # Adding again 1 because of reserved 0 index
@@ -42,10 +67,7 @@ def embedding_for_vocab(filepath, word_index,
   
     return embedding_matrix_vocab
   
-  
-# matrix for vocab: word_index
-embedding_dim = 50
-embedding_matrix_vocab = embedding_for_vocab('glove.6B\glove.6B.50d.txt', tokenizer.word_index, embedding_dim)
-  
-print("Dense vector for first word is => ",
-      embedding_matrix_vocab[1])
+
+def doGloves(embedding_dim):
+    embedding_matrix_vocab = embedding_for_vocab('glove.6B.50d.txt', tokenizer.word_index,embedding_dim)
+    return embedding_matrix_vocab
