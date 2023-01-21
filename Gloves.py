7483e17cd2d7022b49cde30180d4bbacdd2480e7
bahnee <122479653+bahnee@users.noreply.github.com>
2023-01-21 19:57:06 +0100

    updated gloves

--- Gloves.py
+++ Gloves.py
@@ -2,11 +2,9 @@
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
-import ml
+from nltk import LancasterStemmer
   
 file = None
-dataCorpus = None
-dataCorpusDict = None
 
 #To set the file in the main.
 def setFile(fileP):
@@ -20,72 +18,57 @@ def getFile():
 
 def allWords(file):
     dataCorpus = []
-    print(file["textasList"])
     for comment in file["textasList"]:
         for word in comment:
             dataCorpus.append(word)
     return(dataCorpus)
 
-def allWordsSet(allWords):
-    global dataCorpus
+def allWordsSet(allWordsp):
 
     allWordsSet = []
-    for word in allWords:
+    for word in allWordsp:
         if word not in allWordsSet:
             allWordsSet.append(word)
     print("dataCorpus length: " + str(len(allWordsSet)))
-    dataCorpus = allWordsSet
 
-# create the dict.
-def glovesDict():
-    allWordsSet(allWords(file))
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(dataCorpus)
-  
-    # number of unique words in dict.
-    print("Number of unique words in dictionary=",len(tokenizer.word_index))
-    print("Dictionary is = ", tokenizer.word_index)
-    return tokenizer.word_index
- 
-# download glove and unzip it in Notebook.
-#!wget http://nlp.stanford.edu/data/glove.6B.zip
-#!unzip glove*.zip
-  
-# vocab: 'the': 1, mapping of words with
-# integers in seq. 1,2,3..
-# embedding: 1->dense vector
-def embedding_for_vocab(filepath, word_index,embedding_dim):
-    vocab_size = len(word_index) + 1
-      
-    # Adding again 1 because of reserved 0 index
-    embedding_matrix_vocab = np.zeros((vocab_size,embedding_dim))
-  
-    with open(filepath, encoding="utf8") as f:
+    return allWordsSet
+
+
+def doGloves(featureCount):
+    global file
+    
+    #Create empty dict
+    dataCorpus = allWordsSet(allWords(file))
+    # glovesDict = dict.fromkeys(dataCorpus, None)
+    glovesDict = dict()
+    stemmer = LancasterStemmer()
+
+    #Open the txt and then add the vector the of each word, if it exists in the txt.
+    with open('glove.6B.50d.txt', encoding="utf8") as f:
         for line in f:
             word, *vector = line.split()
-            if word in word_index:
-                idx = word_index[word]
-                embedding_matrix_vocab[idx] = np.array(vector, dtype=np.float32)[:embedding_dim]
-  
-    return embedding_matrix_vocab
-
-def glovesCol(embedding_matrix_vocab):
-
-    global file
-    global dataCorpusDict
+            vector = list(map(float, vector))
+            if word in dataCorpus:
+                glovesDict[word] = vector[0:featureCount]
+            if stemmer.stem(word) in dataCorpus:
+                glovesDict[stemmer.stem(word)] = vector[0:featureCount]
+    
     
     #create an array and insert each word2Vec Vector
-    gloves = []
-    for i in embedding_matrix_vocab:
-        #Now create one vector per comment, which is just the average of the words vectors. 
-        gloves.append(sum(i)/len(i))
-    return gloves
+    glovesCol = []
 
-def doGloves(embedding_dim):
-    global file
-    print(file)
-    embedding_matrix_vocab = embedding_for_vocab('glove.6B.50d.txt', glovesDict(),embedding_dim)
-    print(embedding_matrix_vocab)
-    print(glovesCol(embedding_matrix_vocab))
-    
-    return embedding_matrix_vocab
+    count = 0
+    for comment in file["textasList"]:
+        comment2vec = []
+        for word in comment:
+            wordVec = glovesDict[word]
+            try:
+                comment2vec.append(wordVec)
+            except:
+                pass
+        if comment2vec == []:
+            count+=1
+            comment2vec = [[0] * featureCount]
+        glovesCol.append(np.mean(comment2vec, axis = 0))
+
+    file["gloves"] = glovesCol
